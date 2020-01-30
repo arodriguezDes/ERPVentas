0f960004d9c2dd8dd079ba7781b8b3c16aca647f
arodriguezDes <arodriguez7722@misena.edu.co>
2020-01-30 16:59:00 -0500

    Agregando Helper Sql Articulo

---
+++ helper/ArticuloHelper.php
@@ -0,0 +1,61 @@
+<?php 
+
+class ArticuloHelper{
+
+	public function __construct(){
+
+	}
+
+	public function create($articulo){
+		$sql = "INSERT INTO articulo (idcategoria, codigo, nombre, stock, descripcion, imagen, condicion)
+				VALUES ('" . $articulo->getIdCategoria() . "', '" . $articulo->getCodigo() . "',
+						'" . $articulo->getNombre() . "', '" . $articulo->getStock() . "', 
+						'" . $articulo->getDescripcion() . "', '" . $articulo->getImagen() . "', 
+						'1') ";
+		return ejecutarConsulta($sql);
+	}
+
+	public function edit($articulo){
+		$sql = "UPDATE articulo 
+				SET idcategoria = '" . $articulo->getIdCategoria() . "', codigo = '" . $articulo->getCodigo() . "',
+					nombre = '" . $articulo->getNombre() . "', stock = '" . $articulo->getStock() . "',
+					descripcion = '" . $articulo->getDescripcion() . "', imagen = '" . $articulo->getImagen() . "'
+				WHERE idarticulo = '" . $articulo->getIdArticulo() . "' ";
+		return ejecutarConsulta($sql);
+	}
+
+	public function activate($articulo){
+		$sql = "UPDATE articulo SET condicion = '1'
+				WHERE idarticulo = '" . $articulo->getIdArticulo() . "' ";
+		return ejecutarConsulta($sql);
+	}
+
+	public function defuse($articulo){
+		$sql = "UPDATE articulo SET condicion = '0' 
+				WHERE idarticulo = '" . $articulo->getIdArticulo() . "' ";
+		return ejecutarConsulta($sql);
+	}
+
+	public function show($articulo){
+		$sql = "SELECT * FROM articulo 
+				WHERE idarticulo = '" . $articulo->getIdArticulo() . "' ";
+		return ejecutarConsultaSimpleFila($sql);
+	}
+
+	public function findAll(){
+		$sql = "SELECT 
+					a.idarticulo,
+					c.nombre as categoria,
+					a.codigo, 
+		        	a.nombre, 
+		        	a.stock, 
+		        	a.descripcion, 
+		        	a.imagen, 
+		        	a.condicion 
+				FROM articulo a
+				INNER JOIN categoria c ON a.idcategoria = c.idcategoria";
+		return ejecutarConsulta($sql);
+	}	
+}
+
+ ?>
